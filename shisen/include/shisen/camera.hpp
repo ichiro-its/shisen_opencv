9c220655905a77bfbb35a863449f7e949764c9f9
Segara Bhagas Dagsapurwa <segara2410@gmail.com>
2021-03-04 20:40:07 +0700

    fix cpplint problem

--- shisen/include/shisen/camera.hpp
+++ shisen/include/shisen/camera.hpp
@@ -18,8 +18,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#ifndef SHISEN__VIDEO_CAPTURER_HPP_
-#define SHISEN__VIDEO_CAPTURER_HPP_
+#ifndef SHISEN__CAMERA_HPP_
+#define SHISEN__CAMERA_HPP_
 
 #include <opencv2/videoio.hpp>
 #include <rclcpp/rclcpp.hpp>
@@ -72,4 +72,4 @@ private:
 
 }  // namespace shisen
 
-#endif  // SHISEN__VIDEO_CAPTURER_HPP_
+#endif  // SHISEN__CAMERA_HPP_
